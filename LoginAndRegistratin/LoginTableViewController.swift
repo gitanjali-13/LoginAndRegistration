d1021399bd4294c32ea17fafd9033b834f962817
gitanjali Deshmukh <gitaanjalide@gmail.com>
2023-01-05 18:07:50 +0530

    Initial Commit

---
+++ LoginAndRegistratin/LoginTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginTableViewController.swift
+//  LoginAndRegistratin
+//
+//  Created by Admin on 04/01/23.
+//
+
+import UIKit
+
+class LoginTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return UIScreen.main.bounds.height
+//    }
+//
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        let tableViewsHeight = self.tableView.frame.height
+//        let contentHeight = self.tableView.contentSize.height
+//        let centeringInset = (tableViewsHeight - contentHeight) / 2.0
+//        let topInset = max(centeringInset, 0.0)
+//        self.tableView.contentInset = UIEdgeInsets(top: topInset, left: 0.0, bottom: 0.0, right: 0.0)
+//    }
+
+    
+}
